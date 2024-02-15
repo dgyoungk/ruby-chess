ed2a4ab32c4b1b4fd7d2463b68e4c236a36375fb
Daniel <150309766+dgyoungk@users.noreply.github.com>
2024-02-14 19:16:56 -0800

    chore: add methods for piece moves and chessboard

--- lib/board.rb
+++ lib/board.rb
@@ -14,7 +14,7 @@ class Board
   end
 
   def create_board
-    @coordinates.each do |pair|
+    coordinates.each do |pair|
       new_square = Node.new(pair)
       add_square(new_square)
     end
@@ -22,6 +22,21 @@ class Board
 
   def add_square(new_square)
     squares[new_square.coords] = new_square
+  end
+
+  def attach_board_edges
+    squares.each do |coord, node|
+      possible_edges.each do |pair|
+        unless squares[[coord.first + pair.first, coord.last + pair.last]].nil?
+          add_edge(coord, [coord.first + pair.first, coord.last + pair.last])
+        end
+      end
+    end
+  end
+
+  def add_edge(node1, node2)
+    squares[node1].add_neighbour(squares[node2])
+    squares[node2].add_neighbour(squares[node1])
   end
 
 end
--- lib/modules/retrievable.rb
+++ lib/modules/retrievable.rb
@@ -8,4 +8,29 @@ module Retrievable
   def board_coordinates
     return (0..7).to_a.repeated_permutation(2).to_a
   end
+
+  def moves_of_knight
+    base =  (-2..2).to_a.repeated_permutation(2).to_a
+    return base.reject { |pair| pair.first.eql?(pair.last) || pair.include?(0) || pair.first.eql?(-(pair.last)) }
+  end
+
+  def moves_of_pawn
+    return [[0, 1], [-1, 1], [1, 1]]
+  end
+
+  def moves_of_rook
+    base = (-6..6).to_a.repeated_permutation(2).to_a.select do |pair|
+      unless pair.first.eql?(pair.last)
+        pair.first.zero? || pair.last.zero?
+      end
+    end
+  end
+
+  def moves_of_bishop
+    base = (-6..6).to_a.repeated_permutation(2).to_a.select do |pair|
+      unless pair.first.zero?
+        pair.first.eql?(pair.last) || pair.first.eql?(-pair.last)
+      end
+    end
+  end
 end
