68ef331a7338d675cca5ce2eaa1a739db69c9d88
Daniel <150309766+dgyoungk@users.noreply.github.com>
2024-03-19 16:20:22 -0700

    chore: move gameplay methods to a module

--- lib/game.rb
+++ lib/game.rb
@@ -43,11 +43,7 @@ class Game
   def create_player(count)
     # p_name = refine_name(count)
     p_name = players.empty? ? 'joe' : 'beck'
-    assign_player(p_name)
-  end
-
-  def assign_player(username)
-    players.empty? ? new_player(username) : new_player(username)
+    new_player(p_name)
   end
 
   def new_player(username)
@@ -69,184 +65,19 @@ class Game
   def play_once
     until game_over?
       players.each do |player|
+        other_player = player.piece_color.eql?('white') ? players.last : players.first
         break if game_draw_status(player)
         show_chess_board(self.board)
         moving_info_msg
         turn_msg(turn)
-        move_piece(player, turn)
-        break if check_game_status(player)
+        move_piece(player, turn, board, other_player)
+        break if check_game_status(player, other_player)
       end
       self.turn += 1
     end
   end
 
-  def move_piece(player, turn)
-    player_pieces = board.squares.values.select { |spot| spot.occupied_by.color.eql?(player.piece_color) }
-    move_notation = piece_position(player).split(/,\s*/)
-    move_notation = square_occupancy(move_notation, player)
-    move_notation, temp_piece = moving_while_check(move_notation, player_pieces, player)
-    move_notation, temp_piece = legal_move_review(player, move_notation, temp_piece, player_pieces)
-    move_or_capture(player, move_notation, temp_piece)
-  end
-
-  def filter_move(move_notation, player_pieces, player)
-    move_notation, temp_piece = correct_piece(player_pieces, move_notation, player)
-    until legal_move?(move_notation, temp_piece)
-      illegal_move_msg
-      move_notation = piece_position(player).split(/,\s*/)
-      move_notation, temp_piece = correct_piece(player_pieces, move_notation, player)
-    end
-    return move_notation, temp_piece
-  end
-
-  def square_occupancy(move_notation, player)
-    destination = piece_destination(move_notation)
-    while board.squares[destination].occupied_by.color.eql?(player.piece_color)
-      square_occupied_msg
-      move_notation = piece_position(player).split(/,\s*/)
-      destination = piece_destination(move_notation)
-    end
-    move_notation
-  end
-
-  def move_or_capture(player, move_notation, temp_piece)
-    other_player = player.piece_color.eql?('white') ? players.last : players.first
-    destination = piece_destination(move_notation)
-    if empty_spot?(destination, board)
-      piece_moving(move_notation, player, temp_piece)
-    elsif board.squares[destination].occupied_by.color.eql?(other_player.piece_color)
-      piece_capturing(move_notation, player, temp_piece)
-    end
-  end
-
-  def piece_moving(move_notation, player, temp_piece)
-    case temp_piece.occupied_by.type
-    when 'knight'
-      swap_places(move_notation, temp_piece)
-    when 'king'
-      move_king_piece(move_notation, player, temp_piece)
-    when 'pawn'
-      move_pawn_piece(move_notation, player, temp_piece)
-    else
-      move_other_piece(move_notation, player, temp_piece)
-    end
-  end
-
-  def move_king_piece(move_notation, player, temp_piece)
-    destination = piece_destination(move_notation)
-    other_player = player.piece_color.eql?('white') ? players.last : players.first
-    swap_places(move_notation, temp_piece)
-    while check?(board, other_player)
-      check_move_msg
-      temp_piece.add_occupancy(board.squares[destination].occupied_by)
-      add_blank_spot(board.squares[destination])
-      move_notation = piece_position(player).split(/,\s*/)
-      swap_places(move_notation, temp_piece)
-    end
-  end
-
-  def moving_while_check(move_notation, player_pieces, player)
-    move_notation, temp_piece = filter_move(move_notation, player_pieces, player)
-    other_player = player.piece_color.eql?('white') ? players.last : players.first
-    # binding.pry
-    loop do
-      return move_notation, temp_piece if piece_type(move_notation).eql?('king')
-      temp_board = Marshal.load(Marshal.dump(board))
-      piece_copy = Marshal.load(Marshal.dump(temp_piece))
-      destination = piece_destination(move_notation)
-      pseudo_swap(temp_board, piece_copy, destination)
-      break unless check?(temp_board, other_player)
-      forfeit_move_msg
-      move_notation = piece_position(player).split(/,\s*/)
-      move_notation, temp_piece = filter_move(move_notation, player_pieces, player)
-    end
-    return move_notation, temp_piece
-  end
-
-  # this method will prevent a player from making a move that will put the king piece in check
-  def legal_move_review(player, move_notation, temp_piece, player_pieces)
-    other_player = player.piece_color.eql?('white') ? players.last : players.first
-    # binding.pry
-    loop do
-      return move_notation, temp_piece if piece_type(move_notation).eql?('king')
-      temp_board = Marshal.load(Marshal.dump(board))
-      piece_copy = Marshal.load(Marshal.dump(temp_piece))
-      destination = piece_destination(move_notation)
-      pseudo_swap(temp_board, piece_copy, destination)
-      break unless check?(temp_board, other_player)
-      check_move_msg
-      move_notation = piece_position(player).split(/,\s*/)
-      move_notation, temp_piece = filter_move(move_notation, player_pieces, player)
-    end
-    return move_notation, temp_piece
-  end
-
-  def pseudo_swap(temp_board, piece_copy, destination)
-    temp_square = temp_board.squares[destination].occupied_by
-    temp_board.squares[destination].add_occupancy(piece_copy.occupied_by)
-    temp_board.squares[piece_copy.coords].add_occupancy(temp_square)
-  end
-
-  def move_pawn_piece(move_notation, player, temp_piece)
-    starting_pos = player.piece_color.eql?('white') ? 7 : 2
-    non_capture_moves = pawn_allowed_moves(starting_pos, temp_piece)
-    move_notation = legal_pawn_not(move_notation, non_capture_moves, player, temp_piece)
-    # what are the conditions for promoting? I need the piece color and the row number
-    temp_piece = pawn_promote_review(player, temp_piece, move_notation)
-    swap_places(move_notation, temp_piece)
-  end
-
-  def move_other_piece(move_notation, player, temp_piece)
-    until clear_path?(move_notation, player, board)
-      blocked_path_msg
-      move_notation = piece_position(player).split(/,\s*/)
-    end
-    destination = piece_destination(move_notation)
-    swap_places(move_notation, temp_piece)
-  end
-
-  def piece_capturing(move_notation, player, temp_piece)
-    case temp_piece.occupied_by.type
-    when 'knight', 'king'
-      capture_piece(move_notation, player, temp_piece)
-    when 'pawn'
-      pawn_capture(move_notation, player, temp_piece)
-    else
-      non_knight_capture(move_notation, player, temp_piece)
-    end
-  end
-
-  def non_knight_capture(move_notation, player, spot)
-    until clear_path?(move_notation, player, board)
-      blocked_path_msg
-      move_notation = piece_position(player).split(/,\s*/)
-    end
-    capture_piece(move_notation, player, spot)
-  end
-
-  def pawn_capture(move_notation, player, temp_piece)
-    capturing_moves = color_specific_captures(temp_piece)
-    move_notation = legal_pawn_not(move_notation, capturing_moves, player, temp_piece)
-    temp_piece = pawn_promote_review(player, temp_piece, move_notation)
-    capture_piece(move_notation, player, temp_piece)
-  end
-
-  def swap_places(move_notation, spot)
-    destination = piece_destination(move_notation)
-    board.squares[destination].add_occupancy(spot.occupied_by)
-    add_blank_spot(spot)
-  end
-
-  def capture_piece(move_notation, player, spot)
-    destination = piece_destination(move_notation)
-    captured = board.squares[destination].occupied_by
-    player.update_captured(captured)
-    swap_places(move_notation, spot)
-  end
-
-  def check_game_status(player)
-    other_player = player.piece_color.eql?('white') ? players.last : players.first
-    # binding.pry
+  def check_game_status(player, other_player)
     if checkmate?(board, player, other_player)
       self.game_finished = true
       winner_msg(player.name)
@@ -268,7 +99,7 @@ class Game
 
   def prompt_replay
     replay_choice = user_decision
-    self.replay = false if replay_choice == 'n'
+    self.replay = false if replay_choice.eql?('n')
   end
 
   def game_end
